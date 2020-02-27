146641b0e4d2b07a966fdc21b704f6132b44ae67
Victor Mujica <victor@industriafuturo.com>
2020-02-27 15:52:13 -0600

    zuul config

--- src/main/java/mx/ipn/escom/neuropsigatewayserver/NeuropsiGatewayServerApplication.java
+++ src/main/java/mx/ipn/escom/neuropsigatewayserver/NeuropsiGatewayServerApplication.java
@@ -2,12 +2,16 @@ package mx.ipn.escom.neuropsigatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class NeuropsiGatewayServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NeuropsiGatewayServerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(NeuropsiGatewayServerApplication.class, args);
+  }
 
 }
