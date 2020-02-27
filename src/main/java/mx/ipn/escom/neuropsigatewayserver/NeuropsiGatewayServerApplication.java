f41aa21e1ae4075da97ce25a2e6ad0eaf1d45239
Victor Mujica <victor@industriafuturo.com>
2020-02-27 15:35:22 -0600

    initial

---
+++ src/main/java/mx/ipn/escom/neuropsigatewayserver/NeuropsiGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package mx.ipn.escom.neuropsigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuropsiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuropsiGatewayServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/ipn/escom/neuropsigatewayserver/NeuropsiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.ipn.escom.neuropsigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuropsiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
