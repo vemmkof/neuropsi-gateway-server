dd539e17164e09bcf7650bcff7bdfe8098c327c6
Victor Mujica <victor.mujica.aztech@gmail.com>
2020-04-09 19:52:02 -0500

    test

--- src/test/java/mx/ipn/escom/neuropsigatewayserver/NeuropsiGatewayServerApplicationTests.java
+++ src/test/java/mx/ipn/escom/neuropsigatewayserver/NeuropsiGatewayServerApplicationTests.java
@@ -1,13 +1,15 @@
 package mx.ipn.escom.neuropsigatewayserver;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NeuropsiGatewayServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+    assertThat(this).isNotNull();
+  }
 
 }
